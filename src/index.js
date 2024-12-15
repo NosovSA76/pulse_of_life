7b9b38045b949fbe9bacd3b1377dd0b453f17cfe
NosovSA76 <125302593+NosovSA76@users.noreply.github.com>
2024-12-15 11:50:15 +0200

    test

--- src/index.js
+++ src/index.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
+import { BrowserRouter } from 'react-router-dom';
 // import './index.css';
 import App from './components/App';
 import reportWebVitals from './reportWebVitals';
@@ -7,12 +8,13 @@ import { ThemeProvider } from 'styled-components';
 
 import { theme } from './styles/theme';
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
+ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <ThemeProvider theme={theme}>
-      <App />
-    </ThemeProvider>
+  <BrowserRouter basename="/pulse_of_life">
+        <ThemeProvider theme={theme}>
+          <App />
+        </ThemeProvider>
+      </BrowserRouter>
   </React.StrictMode>
 );
 
